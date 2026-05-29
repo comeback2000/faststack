b76853503c52f880dd37d4252355fda12aa7f3c0
comeback2000 <ranjancom2000@gmail.com>
2026-05-29 22:15:06 +0530

    Point app to authorized Apps Script backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.FASTSTACK_BACKEND = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyLSgHonXya_jxRWIcWWZO40AbusriyPt0WA3gyA8Lu-l1aZBhWApVrpQrXK2vwWf7I/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyblJJ__zHmUawi2LeC6YfyfrswNKBaINI370CQqCHYL2k4Zo3AqI5d9M8MSXH5zmJh/exec",
 };

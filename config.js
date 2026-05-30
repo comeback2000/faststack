d61faa689f36f4013b55872d4f7a23382b886d79
comeback2000 <ranjancom2000@gmail.com>
2026-05-30 15:43:23 +0530

    Point app to working Apps Script backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.FASTSTACK_BACKEND = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyQlFCosXoSfrQhgS2eT2PIAlQsnUIjYKjxpmbaTW4YM08QxHhEbuvIlF_O9DSrJVN3/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbw5pE7-O81TZdFk8mAuWRubLodqqntl14tB6t94l0lrqLhURQlZUck8wzEEtS6OkhHX/exec",
 };
